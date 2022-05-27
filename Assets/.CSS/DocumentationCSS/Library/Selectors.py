7aebfb4e4e3e11984a8c43a3feb89505ce624cb5
DirectiveAthena <directive.athena@gmail.com>
2022-05-27 21:14:26 +0200

    Docu: 2022-05-27 21:14:26
    Affected files:
    Assets/.CSS/DocumentationCSS/Library/Selectors.py

--- Assets/.CSS/DocumentationCSS/Library/Selectors.py
+++ Assets/.CSS/DocumentationCSS/Library/Selectors.py
@@ -16,7 +16,7 @@ from AthenaCSS import (
 # - Code -
 # ----------------------------------------------------------------------------------------------------------------------
 class_markdown_rendered = CSSClass("markdown-rendered")
-class_publish_article_heading = CSSElement(CSSClass("publish-article-heading"))
+class_publish_article_heading = CSSClass("publish-article-heading")
 HEADERS = (ElementLib.H1,ElementLib.H2,ElementLib.H3,ElementLib.H4,ElementLib.H5,ElementLib.H6)
 
 class_adam = CSSClass("adam")
