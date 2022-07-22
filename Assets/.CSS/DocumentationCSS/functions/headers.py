0259535016888564c3b647cfb2e7e58d7c956a36
Andreas Sas <directive.athena@gmail.com>
2022-07-22 18:48:47 +0200

    Docu: 2022-07-22 18:48:47
    Affected files:
    Assets/.CSS/DocumentationCSS/functions/headers.py

--- Assets/.CSS/DocumentationCSS/functions/headers.py
+++ Assets/.CSS/DocumentationCSS/functions/headers.py
@@ -12,7 +12,12 @@ from AthenaLib.HTML.models.html import HTMLElement
 
 # Custom Packages
 from DocumentationCSS.data.comments import LINE
+from DocumentationCSS.data.classes import CLASS_MARKDOWN_EMBED, CLASS_PUBLISH_ARTICLE_HEADING
 
+# ----------------------------------------------------------------------------------------------------------------------
+# - Support Code -
+# ----------------------------------------------------------------------------------------------------------------------
+HEADERS = (HtmlLib.H1(), HtmlLib.H2(), HtmlLib.H3(), HtmlLib.H4(), HtmlLib.H5(), HtmlLib.H6())
 # ----------------------------------------------------------------------------------------------------------------------
 # - Code -
 # ----------------------------------------------------------------------------------------------------------------------
@@ -23,6 +28,14 @@ def header_default():
     yield LINE
 
     # actual rules
+    for h in HEADERS:
+        h_new = h.
     yield CSSRule(
-
+        selections=(
+            *(CSSSelection(HTMLElement(classes=CLASS_MARKDOWN_EMBED), h,selector_type=CSSSelectionType.inside)
+              for h in HEADERS),
+            *(CSSSelection(HTMLElement(classes=CLASS_MARKDOWN_EMBED), , selector_type=CSSSelectionType.inside)
+              for h in HEADERS),
+        ),
+        properties=()
     )
