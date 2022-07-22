69e602d276df9af974a5a2d0dee1086c4abf1a40
Andreas Sas <directive.athena@gmail.com>
2022-07-22 18:38:45 +0200

    Docu: 2022-07-22 18:38:45
    Affected files:
    Assets/.CSS/DocumentationCSS/data/comments.py
    Assets/.CSS/DocumentationCSS/functions/headers.py

--- Assets/.CSS/DocumentationCSS/data/comments.py
+++ Assets/.CSS/DocumentationCSS/data/comments.py
@@ -4,25 +4,11 @@
 # General Packages
 
 # Custom Library
-from AthenaColor import RGB
-from AthenaCSS.models.generator import CSSGenerator
-from AthenaLib.HTML.models.css import CSSProperty, CSSComment, CSSRule, CSSSelection, CSSSelectionType
-import AthenaLib.HTML.models.html_library as HtmlLib
-from AthenaLib.HTML.models.html import HTMLElement
+from AthenaLib.HTML.models.css import CSSComment
 
 # Custom Packages
-from DocumentationCSS.data.comments import LINE
 
 # ----------------------------------------------------------------------------------------------------------------------
 # - Code -
 # ----------------------------------------------------------------------------------------------------------------------
-def header_default() -> CSSRule|CSSComment:
-    # comment structure
-    yield LINE
-    yield CSSComment("- All headers derive from this -")
-    yield LINE
-
-    # actual rules
-    yield CSSRule(
-
-    )
+LINE = CSSComment("-"*64)
