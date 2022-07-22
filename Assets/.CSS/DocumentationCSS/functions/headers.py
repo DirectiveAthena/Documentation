300728a29118c6269aa009d62ed5bce68fc29cd4
Andreas Sas <directive.athena@gmail.com>
2022-07-22 18:58:47 +0200

    Docu: 2022-07-22 18:58:47
    Affected files:
    Assets/.CSS/DocumentationCSS/data/gradients.py
    Assets/.CSS/DocumentationCSS/dump.css
    Assets/.CSS/DocumentationCSS/functions/headers.py

--- Assets/.CSS/DocumentationCSS/functions/headers.py
+++ Assets/.CSS/DocumentationCSS/functions/headers.py
@@ -11,15 +11,18 @@ from AthenaCSS.models.generator import CSSGenerator
 from AthenaLib.HTML.models.css import CSSProperty, CSSComment, CSSRule, CSSSelection, CSSSelectionType
 import AthenaLib.HTML.models.html_library as HtmlLib
 from AthenaLib.HTML.models.html import HTMLElement
+from AthenaCSS.models.athenalib_imports import Pixel, RootElementFontSize
 
 # Custom Packages
 from DocumentationCSS.data.comments import LINE
 from DocumentationCSS.data.classes import CLASS_MARKDOWN_EMBED, CLASS_PUBLISH_ARTICLE_HEADING
+from DocumentationCSS.data.gradients import GRADIENT_HEADER
 
 # ----------------------------------------------------------------------------------------------------------------------
 # - Support Code -
 # ----------------------------------------------------------------------------------------------------------------------
 HEADERS = (HtmlLib.H1, HtmlLib.H2, HtmlLib.H3, HtmlLib.H4, HtmlLib.H5, HtmlLib.H6)
+
 # ----------------------------------------------------------------------------------------------------------------------
 # - Code -
 # ----------------------------------------------------------------------------------------------------------------------
@@ -45,5 +48,34 @@ def header_default():
             ) for h in HEADERS
             ),
         ),
-        properties=()
+        properties=(
+            CSSProperty("text-align","left"),
+            CSSProperty("background-color",RGB(25,25,25)),
+            CSSProperty("background-image",GRADIENT_HEADER),
+            CSSProperty("border-radius",RootElementFontSize(0.2)),
+            CSSProperty("border-bottom-style","solid"),
+            CSSProperty("border-bottom-width",Pixel(5)),
+        )
+    ) # actual rules
+    yield CSSRule(
+        selections=(
+            CSSSelection(
+                HTMLElement(classes=CLASS_MARKDOWN_EMBED),
+                HtmlLib.H1(),
+                selector_type=CSSSelectionType.inside
+            ),
+            CSSSelection(
+                HTMLElement(classes=CLASS_MARKDOWN_EMBED),
+                HtmlLib.H1(classes=CLASS_PUBLISH_ARTICLE_HEADING),
+                selector_type=CSSSelectionType.inside
+            )
+        ),
+        properties=(
+            CSSProperty("text-align","left"),
+            CSSProperty("background-color",RGB(25,25,25)),
+            CSSProperty("background-image",GRADIENT_HEADER),
+            CSSProperty("border-radius",RootElementFontSize(0.2)),
+            CSSProperty("border-bottom-style","solid"),
+            CSSProperty("border-bottom-width",Pixel(5)),
+        )
     )
