b63774620da804de09c9ad699603264f04476d7f
Andreas Sas <directive.athena@gmail.com>
2022-07-23 22:41:01 +0200

    Docu: 2022-07-23 22:41:01
    Affected files:
    Assets/.CSS/DocumentationCSS/data/classes.py
    Assets/.CSS/DocumentationCSS/functions/header_border_colors.py
    Assets/.CSS/DocumentationCSS/functions/kanban.py

--- Assets/.CSS/DocumentationCSS/functions/kanban.py
+++ Assets/.CSS/DocumentationCSS/functions/kanban.py
@@ -4,9 +4,20 @@
 # General Packages
 
 # Custom Library
+from AthenaLib.HTML.models.css import CSSComment
 
 # Custom Packages
+from DocumentationCSS.data.comments import LINE
+from DocumentationCSS.data.colors import COLORS
 
 # ----------------------------------------------------------------------------------------------------------------------
 # - Code -
 # ----------------------------------------------------------------------------------------------------------------------
+def kanban():
+    # comment structure
+    yield LINE
+    yield CSSComment("- Special Rules for the kanban plugin -")
+    yield LINE
+
+    # actual rules
+    yield ".kanban-plugin {background-color: var(--background-primary);"
