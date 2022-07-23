9bf3e53e66af0d15ac7db80583d6203303e99126
Andreas Sas <directive.athena@gmail.com>
2022-07-23 08:40:55 +0200

    Docu: 2022-07-23 08:40:55
    Affected files:
    Assets/.CSS/DocumentationCSS/data/colors.py
    DailyNotes/2022.07.23.md

--- Assets/.CSS/DocumentationCSS/data/colors.py
+++ Assets/.CSS/DocumentationCSS/data/colors.py
@@ -22,19 +22,19 @@ HEADER_COLORS = (WHITE, Color.Gainsboro, Color.Silver, Color.DarkGray, Color.Gra
 # ----------------------------------------------------------------------------------------------------------------------
 # - AI colors -
 # ----------------------------------------------------------------------------------------------------------------------
-COLOR_ADAM = RGB(24 , 177, 222)
-COLOR_ATHENA = RGB(196, 41 , 80 )
-COLOR_EVA = RGB(234, 39 , 79 )
-COLOR_JUPITER = RGB(235, 233, 67 )
-COLOR_VERITAS = RGB(213, 55 , 146)
-COLOR_VULCANUS = RGB(236, 133, 33 )
-COLOR_NEPTUNE = RGB(66 , 115, 185)
-COLOR_MINERVA = RGB(105, 59 , 144)
-COLOR_SOL = RGB(246, 231, 134)
-COLOR_AERO = RGB(251, 251, 251)
-COLOR_VENUS = RGB(115, 195, 125)
-COLOR_PLUTO = RGB(84 , 85 , 87 )
-COLOR_CERES = RGB(213, 224, 68 )
+COLOR_ADAM = RGB(24,177,222)
+COLOR_ATHENA = RGB(196,41,80)
+COLOR_EVA = RGB(234,39,79)
+COLOR_JUPITER = RGB(235,233,67)
+COLOR_VERITAS = RGB(213,55,146)
+COLOR_VULCANUS = RGB(236,133,33)
+COLOR_NEPTUNE = RGB(66,115,185)
+COLOR_MINERVA = RGB(105,59,144)
+COLOR_SOL = RGB(246,231,134)
+COLOR_AERO = RGB(251,251,251)
+COLOR_VENUS = RGB(115,195,125)
+COLOR_PLUTO = RGB(84,85,87)
+COLOR_CERES = RGB(213,224,68)
 
 AI_COLORS = (
     COLOR_ADAM,COLOR_AERO,COLOR_ATHENA,COLOR_CERES,COLOR_EVA,COLOR_JUPITER,COLOR_MINERVA,COLOR_NEPTUNE,COLOR_PLUTO,
@@ -47,9 +47,9 @@ AI_COLORS = (
 # ----------------------------------------------------------------------------------------------------------------------
 # - Website Name colors -
 # ----------------------------------------------------------------------------------------------------------------------
-COLOR_DISCORD = RGB(255, 0  , 0  )
-COLOR_TWITCH = RGB(169, 112, 255)
-COLOR_TWITTER = RGB(26 , 140, 216)
-COLOR_YOUTUBE = RGB(86 , 98 , 246)
+COLOR_DISCORD = RGB(255,0 ,0 )
+COLOR_TWITCH = RGB(169,112,255)
+COLOR_TWITTER = RGB(26,140,216)
+COLOR_YOUTUBE = RGB(86,98,246)
 
 WEBSITE_NAME_COLORS = (COLOR_DISCORD,COLOR_TWITCH,COLOR_TWITTER,COLOR_YOUTUBE,)
